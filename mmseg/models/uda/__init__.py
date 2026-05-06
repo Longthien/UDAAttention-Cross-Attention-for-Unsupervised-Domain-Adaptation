d794384a001099d65ea36f6b4ec5c6da759c053c
Thien <buithien801@gmail.com>
2026-05-06 19:09:48 +0000

    add tada

--- mmseg/models/uda/__init__.py
+++ mmseg/models/uda/__init__.py
@@ -4,4 +4,5 @@ from mmseg.models.uda.dacs import DACS
 from mmseg.models.uda.udaneck_dacs import UDANeck_DACS
 from mmseg.models.uda.nce_dacs import NCE_DACS
 from mmseg.models.uda.ssda_tada import SSDA_TADA
-__all__ = ['DACS', 'UDANeck_DACS', 'NCE_DACS', 'SSDA_TADA']
+from mmseg.models.uda.tada_dacs import TADA_DACS
+__all__ = ['DACS', 'UDANeck_DACS', 'NCE_DACS', 'SSDA_TADA', 'TADA_DACS']
